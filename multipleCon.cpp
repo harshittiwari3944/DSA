ccbd5ef2dd3e085fbf14f32ed4a1663911aa0712
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-18 12:09:16 +0530

    question

---
+++ multipleCon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+int main (){
+
+    int a ; 
+    cout<<" enter an  integer";
+    cin>> a; 
+
+
+    if(a >= 100 && a<= 999 ){
+        cout<< " It is a three digit number ";
+
+    }
+    else{
+        cout<<" its not a three digit number ";
+    }
+
+
+
+    return 0 ;
+}
