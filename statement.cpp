dd63dd3fd84edad5a9e4eedc2faa822a4b86fdff
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-18 11:55:43 +0530

    updated

--- statement.cpp
+++ statement.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main (){
 
+int cost_price;
+int selling_price;
+
+cout<<" Enter the cost price ";
+cin >> cost_price;
+
+cout<< " Enter the selling price ";
+cin>> selling_price;
+
+if (cost_price<selling_price){
+    cout<<" profit of :" << selling_price- cost_price;
+}
+
+else if (cost_price> selling_price)
+{
+    cout<<" loss of : "<<cost_price - selling_price;
+
+}
+else {
+    cout<<"none " << cost_price - selling_price;
+
+}
+
+
 
 
     return 0;
