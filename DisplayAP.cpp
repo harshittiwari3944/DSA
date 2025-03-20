7d40d181ba918c21f24923ddf928905b9350370f
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-20 14:51:42 +0530

    Question for Ap solver

---
+++ DisplayAP.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    int n ; 
+    cout<<" enter the value of n: " ;
+    cin >> n;
+    for (int i = 3; i<= 2*n-1; i+= 2){
+
+        cout<<i << endl ;
+    }
+
+
+
+    return 0 ;
+}
