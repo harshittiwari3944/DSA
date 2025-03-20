911676aeafc1e004fba5ebcdcf6d2611a7c876f8
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-20 15:00:16 +0530

    question practice on loops

---
+++ AllOdd.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+
+    int n ;
+    cout<<" Enter the value of n:  ";
+    cin >> n ; 
+    for(int i = 0 ; i<=n ; i++){
+
+        if(i%2 != 0){
+
+            cout<<i<<endl;
+        }
+    }
+
+
+    return  0 ;
+}
