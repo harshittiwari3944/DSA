a8a0d19e9661c1d276ada753cb9c44d793481e15
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-20 15:15:32 +0530

    question

---
+++ loopsAssignment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+int main (){
+    int  n=100;
+    for(int i =3; i<=n; i++){
+      if(i%3==0){
+
+        cout<<i<<endl;
+      }
+    }
+    return 0;
+}
