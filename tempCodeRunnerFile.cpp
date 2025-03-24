bdc99ce729b4ebe8bf635eb998366211fc016017
harshittiwari3944 <harshittiwari980len@gmail.com>
2025-03-24 12:15:17 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,37 @@
-;
+#include<iostream >
+using namespace std; 
+void print_dream(int rows){
+if (rows<=0){
+return ;
+for (int  i = 0 ; i<2*rows-1;i++){
+if (j<i){
+cout<<" " ;
 }
-int main(){
+else{
+cout<<"*";
+}
+}
+}
+ else{
+        for (int j = 0 ; j<1+1; j ++ ){
+ if ( j <2*rows- i-2}
+                 cout<<"  ";
+ }
+else{
+cout<<" *";
+}
+}
+}
+cout<<endl;
+}
+cout<<endl;
+}
+
+int main (){
+  int rows = 5;; 
+
+  print_dream(row)
+  
+
+return 0 ;
+}
