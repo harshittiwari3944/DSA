1f9178a64ca4dad8296d1095b1e6cf2bfbf4deb1
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-03-24 23:12:13 +0530

    hour glass. cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,37 @@
-;
+#include <iostream>
+using namespace std;
+
+void print_hourglass(int rows) {
+    if (rows <= 0) {
+        return;
+    }
+
+    // Top half of the hourglass
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";  // Print leading spaces
+        }
+        for (int j = 0; j < 2 * (rows - i) - 1; j++) {
+            cout << "*";  // Print stars
+        }
+        cout << endl;
+    }
+
+    // Bottom half of the hourglass
+    for (int i = rows - 2; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";  // Print leading spaces
+        }
+        for (int j = 0; j < 2 * (rows - i) - 1; j++) {
+            cout << "*";  // Print stars
+        }
+        cout << endl;
+    }
 }
-int main(){
+
+int main() {
+    int rows = 5;  // Adjust this value for a larger or smaller hourglass
+    print_hourglass(rows);
+
+    return 0;
+}
