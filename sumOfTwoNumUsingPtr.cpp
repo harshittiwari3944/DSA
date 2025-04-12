e513bdff090d28cbd3203e2148b53b4ba7cc4dac
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-04-13 01:22:14 +0530

    sum problem in ponter

---
+++ sumOfTwoNumUsingPtr.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ; 
+int main (){
+//int x= 5;
+//int y =7;
+//int*ptr1=&x;
+//int*ptr2=&y;
+
+//cout<<*ptr1+*ptr2;
+
+//user input using pointer 
+
+int x;
+int y;
+ 
+int*ptr1=&x;
+int*ptr2=&y;
+cout<<"enter the first number";
+cin>>*ptr1;
+cout<<"enter the  second number ";
+cin >>*ptr2;
+cout<<*ptr1+*ptr2;
+
+
+
+
+
+
+}
+
