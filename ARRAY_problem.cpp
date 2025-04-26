7bcbdc159ccdbd5dc26f701148f52ad6db397fa3
harshittiwari3944 <harshittitwari980len@gmail.com>
2025-04-26 22:49:40 +0530

    array problem

---
+++ ARRAY_problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i];
+        if (i > 0) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
